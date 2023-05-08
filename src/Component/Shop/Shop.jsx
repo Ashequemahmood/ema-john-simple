eb63c29242eafe3c52df31d56bd7361f14de7a5b
M G Asheque Mahmood <ashequemahmood@Ms-MacBook-Pro.lan>
2023-05-08 20:21:46 +0300

    products-added

--- src/Component/Shop/Shop.jsx
+++ src/Component/Shop/Shop.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css';
+import Product from '../Product/Product';
 
 const Shop = () => {
+    const [products, setProducts]= useState([]);
+
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[])
+
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h2>products</h2>
+                {
+                    products.map(product=> <Product
+                        key={product.id}
+                        product= {product}
+                    ></Product>)
+                }
             </div>
             <div className="order-container">
                 <h4>Order Summery</h4>
